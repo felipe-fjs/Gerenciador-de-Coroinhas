0984b0114f2d58e2b3d1e79cef50f60cf6f74c99
felipe-fjs <felipejoaquimtech@gmail.com>
2026-03-04 19:37:34 -0300

    abertura do projeto spring initializer

---
+++ src/main/java/com/felipejoaquim/Gerenciador/de/Coroinhas/GerenciadorDeCoroinhasApplication.java
@@ -0,0 +1,13 @@
+package com.felipejoaquim.Gerenciador.de.Coroinhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeCoroinhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeCoroinhasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipejoaquim/Gerenciador/de/Coroinhas/GerenciadorDeCoroinhasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipejoaquim.Gerenciador.de.Coroinhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeCoroinhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
