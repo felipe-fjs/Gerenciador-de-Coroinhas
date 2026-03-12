41a15877f16a49abf3b5c43b9628912df9be05a9
felipe-fjs <felipejoaquimtech@gmail.com>
2026-03-12 14:24:06 -0300

    wip: instanciando funções do ComunidadeService

--- src/main/java/com/felipejoaquim/gerenciador_de_coroinhas/service/ComunidadeService.java
+++ src/main/java/com/felipejoaquim/gerenciador_de_coroinhas/service/ComunidadeService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.felipejoaquim.gerenciador_de_coroinhas.entity.Comunidade;
 import com.felipejoaquim.gerenciador_de_coroinhas.entity.PerfilFuncao;
+import com.felipejoaquim.gerenciador_de_coroinhas.entity.enums.Roles;
 import com.felipejoaquim.gerenciador_de_coroinhas.repository.ComunidadeRepository;
 import com.felipejoaquim.gerenciador_de_coroinhas.repository.PerfilFuncaoRepository;
 
@@ -17,26 +18,48 @@ public class ComunidadeService {
     @Autowired
     private PerfilFuncaoRepository perfilFuncaoRepository;
 
-    public Comunidade criarComunidade(String nome) {
-        return null;
+    public Integer criarComunidade(String nome) {
+        if (comunidadeRepository.existsByNome(nome)) {
+            throw new RuntimeException("Nome de comunidade já registrado!");
+        }
+        Comunidade novaComunidade = new Comunidade(null, nome);
+        try {
+            Integer comunidadeId = comunidadeRepository.save(novaComunidade).getId();
+
+            return comunidadeId;
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Erro ao registrar nova comunidade ["+nome+"]!");
+        }
     }
 
     public Comunidade buscarPorId(Integer comunidadeId) {
-        return null;
+        return comunidadeRepository.findById(comunidadeId).orElseThrow(
+            () -> new RuntimeException("Comunidade de ID = "+comunidadeId+" não encontrada!")
+        );
     }
 
     public List<PerfilFuncao> coroinhasDaComunidade(Integer comunidadeId) {
         // retorna os coroinhas de uma determinada comunidade
-        return null;
+        if (comunidadeRepository.existsById(comunidadeId)){
+            Comunidade comunidade = comunidadeRepository.findById(comunidadeId).get();
+
+            List<PerfilFuncao> coroinhas =  perfilFuncaoRepository.findByComunidadeAndFuncaoAndAtivoTrue(comunidade, Roles.ROLE_COROINHA);
+
+            return coroinhas;
+        }
+        throw new RuntimeException("Comunidade de ID = "+comunidadeId+" não encontrada!");
     }
 
     public List<PerfilFuncao> articuladoresDaComunidade(Integer comunidadeId) {
         // retorna os articuladores de uma determinada comunidade
-        return null;
+        if (comunidadeRepository.existsById(comunidadeId)){
+            Comunidade comunidade = comunidadeRepository.findById(comunidadeId).get();
+
+            List<PerfilFuncao> articuladores =  perfilFuncaoRepository.findByComunidadeAndFuncaoAndAtivoTrue(comunidade, Roles.ROLE_ARTICULADOR);
+
+            return articuladores;
+        }
+        throw new RuntimeException("Comunidade de ID = "+comunidadeId+" não encontrada!");
     }
 
-    public List<Comunidade> buscarPorNome(String nome) {
-        // usar o containing do repository
-        return null;
-    }
 }
