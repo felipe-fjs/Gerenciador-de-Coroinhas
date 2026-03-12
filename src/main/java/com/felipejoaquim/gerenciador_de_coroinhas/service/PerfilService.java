37f6d08055fc63cc740a3c29a203b9f848478e12
felipe-fjs <felipejoaquimtech@gmail.com>
2026-03-12 10:16:52 -0300

    WIP: corrigindo snake_case para camelCase

--- src/main/java/com/felipejoaquim/gerenciador_de_coroinhas/service/PerfilService.java
+++ src/main/java/com/felipejoaquim/gerenciador_de_coroinhas/service/PerfilService.java
@@ -11,13 +11,13 @@ public class PerfilService {
     @Autowired
     private UsuarioRepository usuarioRepository;
         // atualizar para receber DTO
-    public Integer novo_perfil(String usuario_id, Perfil novoPerfil){
+    public Integer novoPerfil(String usuarioId, Perfil novoPerfil){
         // criar perfil
         return null;
     }
 
         // alterar para receber DTO
-    public void editar_perfil(String email, Perfil perfil_atualizado) { // ou buscar por uuid do usuário
+    public void editarPerfil(String email, Perfil perfilAtualizado) { // ou buscar por uuid do usuário
         // verificar o que estiver preenchido do perfil_atualiado e atualizar no perfil registrado
     }
     
