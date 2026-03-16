7efeb384280ada25dca2a10c7532fa272e474605
felipe-fjs <felipejoaquimtech@gmail.com>
2026-03-16 11:00:08 -0300

    wip: adicionando novos DTOs

--- src/main/java/com/felipejoaquim/gerenciador_de_coroinhas/dto/CadastroCompletoDTO.java
+++ src/main/java/com/felipejoaquim/gerenciador_de_coroinhas/dto/CadastroCompletoDTO.java
@@ -1,17 +1,16 @@
 package com.felipejoaquim.gerenciador_de_coroinhas.dto;
 
 import java.time.LocalDate;
-import java.util.List;
 
 
-public record PerfilCompletoResponseDTO(
+public record CadastroCompletoDTO(
                         String nome, 
                         String email,
                         LocalDate dataNascmento,
                         String telefone,
                         String imgUrl, 
                         String usuarioId,
-                        List<PerfilFuncaoDTO> funcoes) { 
+                        String senha) { 
                             // devo colocar uma lista das Funcoes ou devo deixar isso por parte de um "FuncoesDTO"?
 
 }
---
+++ src/main/java/com/felipejoaquim/gerenciador_de_coroinhas/dto/LoginDTO.java
@@ -0,0 +1,4 @@
+package com.felipejoaquim.gerenciador_de_coroinhas.dto;
+
+public record LoginDTO(String email, String senha) {
+}
