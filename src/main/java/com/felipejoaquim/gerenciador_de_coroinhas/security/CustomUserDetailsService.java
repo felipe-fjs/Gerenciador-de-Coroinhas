b4f9920da23b8ce9771cad78c8b073edad2dd7c6
felipe-fjs <felipejoaquimtech@gmail.com>
2026-03-16 11:26:51 -0300

    Update CustomUserDetailsService.java

--- src/main/java/com/felipejoaquim/gerenciador_de_coroinhas/security/CustomUserDetailsService.java
+++ src/main/java/com/felipejoaquim/gerenciador_de_coroinhas/security/CustomUserDetailsService.java
@@ -10,13 +10,14 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.felipejoaquim.gerenciador_de_coroinhas.entity.PerfilFuncao;
 import com.felipejoaquim.gerenciador_de_coroinhas.entity.Usuario;
 import com.felipejoaquim.gerenciador_de_coroinhas.repository.PerfilFuncaoRepository;
 import com.felipejoaquim.gerenciador_de_coroinhas.repository.UsuarioRepository;
 
-
+@Service
 public class CustomUserDetailsService implements UserDetailsService{
     @Autowired
     private UsuarioRepository userRepository;
@@ -34,9 +35,9 @@ public class CustomUserDetailsService implements UserDetailsService{
 
     public Set<GrantedAuthority> loadUserRoles(Usuario usuario){
         Set<GrantedAuthority> authorities = new HashSet<>();
-
+        // mudar para retornar um MAP, o qual a chave serão os nomes ou IDS das comunidades
         if (usuario.getPerfil() != null){
-            List<PerfilFuncao> funcoes = perfilFuncaoRepository.findByPerfil(usuario.getPerfil());
+            List<PerfilFuncao> funcoes = perfilFuncaoRepository.findByPerfilAndAtivoTrue(usuario.getPerfil());
             for (PerfilFuncao role : funcoes){
                 authorities.add(new SimpleGrantedAuthority(role.getFuncao().name()));
             }
