39aea6638f620e0b4551b888954f11a14b6ad597
felipe-fjs <felipejoaquimtech@gmail.com>
2026-03-25 11:45:10 -0300

    wip: rotas básicas simples

---
+++ src/main/java/com/felipejoaquim/gerenciador_de_coroinhas/controller/AuthenticationController.java
@@ -0,0 +1,98 @@
+package com.felipejoaquim.gerenciador_de_coroinhas.controller;
+
+import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.felipejoaquim.gerenciador_de_coroinhas.dto.CadastroCompletoDTO;
+import com.felipejoaquim.gerenciador_de_coroinhas.dto.LoginDTO;
+import com.felipejoaquim.gerenciador_de_coroinhas.entity.Usuario;
+import com.felipejoaquim.gerenciador_de_coroinhas.repository.UsuarioRepository;
+import com.felipejoaquim.gerenciador_de_coroinhas.security.TokenService;
+import com.felipejoaquim.gerenciador_de_coroinhas.service.PerfilService;
+import com.felipejoaquim.gerenciador_de_coroinhas.service.UsuarioService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+    @Autowired
+    private UsuarioService usuarioService;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Autowired
+    private PerfilService perfilService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private TokenService tokenService;
+    
+
+    @PostMapping("/registro")
+    public ResponseEntity<?> novoUsuario(@RequestBody CadastroCompletoDTO body){ 
+        if (usuarioService.emailCadastrado(body.email())){
+            System.out.println("Email registrado agora: " + body.email());
+            return ResponseEntity.badRequest().body("Email já registrado!");
+        }
+
+        try{
+            Usuario novoUsuario = new Usuario(null, body.email(), body.senha(), null);
+            String novoUsuarioId = usuarioService.novoUsuario(novoUsuario);
+
+            // Perfil novoPerfil = new Perfil(null, body.nome(), body.dataNascmento(), body.telefone(), body.imgUrl(), novoUsuario);
+            // perfilService.novoPerfil(novoUsuarioId, novoPerfil);
+
+            // novoUsuario.setPerfil(novoPerfil);
+            // usuarioRepository.save(novoUsuario);
+            System.out.println("Email registrado agora: " + body.email());
+
+            // return ResponseEntity.created(URI.create("novoUsuarioId")).body("Email ["+ body.email()+"] cadastrado com sucesso!");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body("Erro: " + e.getMessage());
+        }
+        return ResponseEntity.ok().body("Novo usuário ["+ body.email() +"] registrado com sucesso!");
+    }
+    
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginDTO body) {
+        if (usuarioService.emailCadastrado(body.email())){
+            Usuario usuario = usuarioService.buscarPorEmail(body.email());
+            if (passwordEncoder.matches(body.senha(), usuario.getSenha())){
+                String token = tokenService.generateToken(usuario);
+                ResponseCookie cookie = ResponseCookie.from("jwt", token)
+                                            .httpOnly(true)
+                                            .path("/")
+                                            .maxAge(Duration.ofMinutes(10))
+                                            .sameSite("Strict") 
+                                            .build();
+
+                return ResponseEntity.ok()
+                                     .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                                     .body("Login com sucesso!");   
+            }
+        }
+        return ResponseEntity.badRequest().body("Email não registrado!");
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout() {
+
+        ResponseCookie cookie = ResponseCookie.from("jwt", "")
+                                    .httpOnly(true)
+                                    .path("/")
+                                    .maxAge(0)
+                                    .sameSite("Strict") 
+                                    .build();
+
+        return ResponseEntity.ok()
+                                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                                .body("logout com sucesso!");   
+    }
+}
