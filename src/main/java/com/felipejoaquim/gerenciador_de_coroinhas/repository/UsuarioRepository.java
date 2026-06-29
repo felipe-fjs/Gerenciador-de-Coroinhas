d9a8f642cf1667b46fddab8b0e7b305d7098dbaa
felipe-fjs <felipejoaquimtech@gmail.com>
2026-06-29 16:38:56 -0300

    alterando de String para UUID e removendo função

--- src/main/java/com/felipejoaquim/gerenciador_de_coroinhas/repository/UsuarioRepository.java
+++ src/main/java/com/felipejoaquim/gerenciador_de_coroinhas/repository/UsuarioRepository.java
@@ -4,12 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.felipejoaquim.gerenciador_de_coroinhas.entity.Usuario;
 import java.util.Optional;
+import java.util.UUID;
 
 
-public interface UsuarioRepository extends JpaRepository<Usuario, String>{
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID>{
     boolean existsByEmail(String email); // usado para verificações de input ou necessários
 
     Optional<Usuario> findByEmail(String email);
 
-    boolean existsByEmailAndAtivoTrue(String email);
 }
